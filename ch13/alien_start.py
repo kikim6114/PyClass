905f80d7f9d70c2365bcfd6263afac9c0fc2b973
kikim6114 <kikim6114@gmail.com>
2025-04-27 11:33:29 +0900

    update ch13

---
+++ ch13/alien_start.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet."""
+
+    def __init__(self, ai_game):
+        """Initialize the alien and set its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
+
+        # Load the alien image and set its rect attribute.
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width   # 스크린 좌측단과의 여백
+        self.rect.y = self.rect.height  # 스크린 윗단과의 여백
+
+        # Store the alien's exact horizontal position.
+        self.x = float(self.rect.x)
